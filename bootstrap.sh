4c5ef0531d70c0ba32e86a1e68631ee9eeb33ae3
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-01 15:18:29 +0200

    + node.js setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,8 @@ dpkg -i erlang-solutions_1.0_all.deb
 apt-get update 
 apt-get -y install elixir
 
+# install node.js
+#curl --insecure --silent -L https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz  -O
+#tar xf node-v0.12.7-linux-x64.tar.gz -C /usr/local
+#apt-get nodejs
+apt-get nodejs-legacy

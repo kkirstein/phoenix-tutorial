ae4ff037f734be4c74d2541e0aa60edb490f9dec
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-04 10:31:20 +0200

    + setup inotify-tools & check for existing symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,12 +17,18 @@ apt-get -y install elixir
 # install node.js
 curl --insecure --silent -L https://nodejs.org/dist/v0.12.7/node-v0.12.7-linux-x64.tar.gz  -O
 tar xf node-v0.12.7-linux-x64.tar.gz -C /usr/local
-ln -s /usr/local/node-v0.12.7-linux-x64/bin/node /usr/local/bin/node
-ln -s /usr/local/node-v0.12.7-linux-x64/bin/npm /usr/local/bin/npm
+if [ ! -h /usr/local/bin/node ]; then
+	ln -s /usr/local/node-v0.12.7-linux-x64/bin/node /usr/local/bin/node
+fi
+if [ ! -h /usr/local/bin/npm ]; then
+	ln -s /usr/local/node-v0.12.7-linux-x64/bin/npm /usr/local/bin/npm
+fi
 
 #apt-get -y install nodejs-legacy
 
-
 # install PostgreSQL server
 apt-get -y install postgresql
+
+# install inotify-tools
+apt-get -y install inotify-tools
 

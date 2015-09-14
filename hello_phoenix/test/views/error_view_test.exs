e2d0d7ad7406f168fe181c13a64e580a3d454fd8
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-14 11:20:48 +0200

    + play around with tags for unit testing

--- hello_phoenix/test/views/error_view_test.exs
+++ hello_phoenix/test/views/error_view_test.exs
@@ -1,14 +1,18 @@
 defmodule HelloPhoenix.ErrorViewTest do
   use HelloPhoenix.ConnCase, async: true
 
+  @moduletag :error_view_case
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  @tag view_test: "404"
   test "renders 404.html" do
     assert render_to_string(HelloPhoenix.ErrorView, "404.html", []) ==
            "Page not found"
   end
 
+  @tag view_test: "500"
   test "render 500.html" do
     assert render_to_string(HelloPhoenix.ErrorView, "500.html", []) ==
            "Server internal error"
